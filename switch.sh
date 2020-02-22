c9ce51269880bc50d85525f7170cf56c055e6518
anuchitop <anuchitp@thoughtworks.com>
2020-02-22 19:05:03 +0700

    v2.0.0

--- switch.sh
+++ switch.sh
@@ -4,30 +4,67 @@ GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 DARK_GRAY='\033[1;30m'
 NO_COLOR='\033[0m'
-VERSION="$1"
+GO_VERSION="$1"
 
-if [ -n "$VERSION" ]; then
-  if [ -d "go$VERSION" ]; then
-    # print current version
-    OLD=`go version`
-    echo -e $DARK_GRAY"current version:"
-    echo -e $ORANGE$OLD
+switch_go_version(){
+   new_version=$1
+   # print current version
 
-    rm -rf go
-    ln -s go$VERSION go
+   OLD=$(go version)
+   echo -e $DARK_GRAY"current version:"
+   echo -e $ORANGE$OLD
 
-    # print new version
-    NEW=`go version`
-    echo -e $DARK_GRAY"switch to version:"
-    echo -e $GREEN$NEW
-    echo -e $DARK_GRAY"done."
+   ln -sfn $GOPATH/bin/$new_version $GOPATH/bin/go
+
+
+   # print new version
+   NEW=$(go version)
+   echo -e $DARK_GRAY"switch to version:"
+   echo -e $GREEN$NEW
+   echo -e $DARK_GRAY"done."
+
+   exit 0
+}
+
+
+### start program switch ###
+if [ -n "$GO_VERSION" ]; then
+
+  # check if go version already download or not.
+  `$GO_VERSION version`
+
+  if [ $? -eq 0 ]; then
+     switch_go_version $GO_VERSION
   else
-    echo -e $RED"not found folder go$VERSION here."
-  fi
-else
+    read -r -p "$ORANGEdo you want to download $GO_VERSION? [Y/n]" input
+    case $input in
+      [yY][eE][sS]|[yY])
 
+     `go get -v golang.org/dl/$GO_VERSION`
+
+     if [ $? -ne 0 ]; then
+        echo -e $RED"Not found $GO_VERSION on golang.org/dl."
+        exit 1
+     fi
+
+     `$GO_VERSION download`
+
+     switch_go_version $GO_VERSION
+    ;;
+      [nN][oO]|[nN])
+      echo "bye."
+    ;;
+      *)
+      echo -e $DARK_GRAY"exit."
+      exit 1
+    ;;
+   esac
+
+  fi
+
+else
   echo -e $RED"please pass the version."
-  echo -e $RED"example ./switch.sh 1.10."
-  echo -e $RED"script will be find folder go1.10 to change version."
+  echo -e $RED"example ./switch.sh go1.13.8"
+  echo -e $RED"script will be find folder go1.13.8 to change version."
 fi
 
